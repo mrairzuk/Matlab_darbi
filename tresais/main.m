4d6d2245f3b7c66949f909f45ac2682b6de2165e
Jevgenijs Grigorjevs <mrairzuk@gmail.com>
2022-01-06 03:00:49 +0200

    fixed 3rd

--- tresais/main.m
+++ tresais/main.m
@@ -1,11 +1,9 @@
-clc
-
 syms X Y
 % sin(X)+2*Y==2,X
 % cos(Y-1)+X==0.8,Y
 
-G1(Y)=(2-sin(X))/2;
-G2(X)=0.8-cos(Y-1);
+G1(Y)=0.8-cos(Y-1);
+G2(X)=(2-sin(X))/2;
 
 G1X=diff(G1,X)
 G1Y=diff(G1,Y)
@@ -17,8 +15,8 @@ solve(-cos(X)/2==-1)
 solve(sin(Y - 1)==1)
 solve(sin(Y - 1)==-1)
 
-f1=@(x) 2-sin(x)./2;
-f2=@(y) 0.8-cos(y-1);
+f1=@(y) 0.8-cos(y-1);
+f2=@(x) (2-sin(x))./2;
 
 x=linspace(1.8,2,100);
 y=linspace(0,0.4,100);
